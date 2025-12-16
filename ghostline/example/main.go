0cd91823212cd03ac3ee1078f8ea3062624dff7b
Lucas Villarinho <lucasvillarinho1@gmail.com>
2025-12-16 19:42:03 -0300

    feat(ghostline): add Makefile and example main.go for project setup and demonstration

---
+++ ghostline/example/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/termisquad/x/ghostline"
+)
+
+func main() {
+	suggestions := []string{
+		"help",
+		"hello",
+		"history",
+		"exit",
+		"clear",
+		"config",
+		"commit",
+		"checkout",
+	}
+
+	input := ghostline.NewInput(suggestions, nil, nil)
+
+	fmt.Println("Ghostline Demo")
+	fmt.Println("Type and see suggestions. Tab to accept. Ctrl+C to exit.")
+	fmt.Println()
+
+	for {
+		line, ok := input.Readline(">>> ")
+		if !ok {
+			fmt.Println("\nBye!")
+			break
+		}
+
+		if line == "exit" {
+			fmt.Println("Bye!")
+			break
+		}
+
+		fmt.Printf("You typed: %q\n", line)
+	}
+}
